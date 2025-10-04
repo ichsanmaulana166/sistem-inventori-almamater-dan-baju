68c97304b5233a9ba429bbb20f580e1bab1c273a
ichsanmaulana166 <ichsanmaulana166@gmail.com>
2025-10-04 18:32:53 +0700

    add BigFolder

--- README.md
+++
@@ -1,2 +0,0 @@
-# sistem-inventori-almamater-dan-baju
-sistem inventori untuk sekolah berupa almamater dan baju
--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img src="{{ asset('images/logo.png') }}" alt="Logo" style="max-width: 100%; height: auto;" width="116" height="116" />
